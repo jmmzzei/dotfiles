1f8d1e0cc3d228851ddb21e52803e22b6e67161f
José María <josemmazzei@gmail.com>
2023-05-05 22:16:15 -0300

    Add vimwiki config

--- config/nvim/lua/jmm/init.lua
+++ config/nvim/lua/jmm/init.lua
@@ -1,2 +1,25 @@
 require("jmm.remap")
 require("jmm.set")
+
+-- VIMWIKI Config
+-- Must be placed here because of this: https://github.com/vimwiki/vimwiki/issues/1130#issuecomment-1373515122
+local function isOnSomeParticularMachine(hostname)
+	local f = io.popen("hostname")
+	local myhostname = f:read("*a"):gsub("\n", "")
+	f:close()
+	return myhostname == hostname
+end
+
+function GetNotesFolder()
+	if isOnSomeParticularMachine("jmm-pc") then
+		return "~/Documents/notes"
+	else
+		return "~/Documentos/notes"
+	end
+end
+
+vim.g.vimwiki_list = {
+	{ path = GetNotesFolder(), syntax = "markdown", ext = ".md" },
+}
+
+vim.api.nvim_set_keymap("n", "<Leader>ww", ":vs 100 | VimwikiIndex<CR>", {})
