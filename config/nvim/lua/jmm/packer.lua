f76e55002e9767e94efa9413549a80c607710519
José María <josemmazzei@gmail.com>
2023-05-06 21:23:09 -0300

    Configure neo-tree

--- config/nvim/lua/jmm/packer.lua
+++ config/nvim/lua/jmm/packer.lua
@@ -13,8 +13,6 @@ return require("packer").startup(function(use)
 
 	use("tpope/vim-fugitive")
 
-	use("machakann/vim-sandwich")
-
 	use("sheerun/vim-polyglot")
 
 	use("junegunn/goyo.vim")
@@ -74,8 +72,6 @@ return require("packer").startup(function(use)
 
 	use("mbbill/undotree")
 
-	use("tpope/vim-fugitive")
-
 	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
 	use({
@@ -105,7 +101,39 @@ return require("packer").startup(function(use)
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 			"MunifTanjim/nui.nvim",
+			{
+				-- only needed if you want to use the commands with "_with_window_picker" suffix
+				"s1n7ax/nvim-window-picker",
+				tag = "v1.*",
+				config = function()
+					require("window-picker").setup({
+						autoselect_one = true,
+						include_current = false,
+						filter_rules = {
+							-- filter using buffer options
+							bo = {
+								-- if the file type is one of following, the window will be ignored
+								filetype = { "neo-tree", "neo-tree-popup", "notify" },
+
+								-- if the buffer type is one of following, the window will be ignored
+								buftype = { "terminal", "quickfix" },
+							},
+						},
+						other_win_hl_color = "#e35e4f",
+					})
+				end,
+			},
 		},
+		config = function()
+			require("neo-tree").setup({
+				popup_border_style = "single",
+				window = {
+					mappings = {
+						["l"] = "move_cursor_down",
+					},
+				},
+			})
+		end,
 	})
 
 	use("RRethy/vim-illuminate")
