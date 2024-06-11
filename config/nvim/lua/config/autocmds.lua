c0105ee84161bf63b2094970384f4bcda51211d3
José María <josemmazzei@gmail.com>
2024-06-11 00:30:34 -0300

    Add function to open in the default browser the bitbucket PR section based in the repo's origin info

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -23,3 +23,57 @@ vim.cmd("cabbrev Del Delete!")
 
 -- autosave
 vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
+
+-- Function to open in the default browser the pull request section of bitbucket based in the repo's origin info
+
+local function run_shell_command(cmd)
+	local handle = io.popen(cmd)
+	local result = handle:read("*a")
+	handle:close()
+	return result
+end
+
+local function get_bitbucket_remote_url()
+	local remote_url = run_shell_command("git config --get remote.origin.url")
+	return remote_url:gsub("%s+", "") -- Trim any whitespace
+end
+
+local function construct_pr_url(remote_url)
+	local username, repo_name = remote_url:match("bitbucket%.org[:/](.-)/(.-)%.git")
+	if username and repo_name then
+		return string.format("https://bitbucket.org/%s/%s/pull-requests/", username, repo_name)
+	else
+		return nil, "Invalid Bitbucket remote URL."
+	end
+end
+
+local function open_url_in_browser(url)
+	local open_command
+	if vim.fn.has("mac") == 1 then
+		open_command = "open " .. url
+	elseif vim.fn.has("unix") == 1 then
+		open_command = "xdg-open " .. url
+	elseif vim.fn.has("win32") == 1 then
+		open_command = "start " .. url
+	else
+		print("Unsupported OS")
+		return
+	end
+	os.execute(open_command)
+end
+
+local function get_pr_url()
+	local remote_url = get_bitbucket_remote_url()
+	if remote_url == "" then
+		print("No remote URL found.")
+		return
+	end
+	local pr_url, err = construct_pr_url(remote_url)
+	if pr_url then
+		open_url_in_browser(pr_url)
+	else
+		print(err)
+	end
+end
+
+vim.api.nvim_create_user_command("PR", get_pr_url, {})
